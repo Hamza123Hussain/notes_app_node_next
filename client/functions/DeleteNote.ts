744957847bcf1ad8a2453b9f4f1e6e9d48d0041c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-05 20:01:18 +0500

    url updated

--- client/functions/DeleteNote.ts
+++ client/functions/DeleteNote.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const DeleteNote = async (_id: any) => {
   try {
     const Response = await axios.delete(
-      `http://localhost:5000/api/Notes/${_id}`
+      `https://notes-app-node-next.vercel.app/api/Notes/${_id}`
     )
     if (Response.status === 200) {
       return true
--- client/functions/GetNotes.ts
+++ client/functions/GetNotes.ts
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export const GetNotes = async (Email: any) => {
   try {
-    const Response = await axios.get(`http://localhost:5000/api/Notes/${Email}`)
+    const Response = await axios.get(
+      `https://notes-app-node-next.vercel.app/api/Notes/${Email}`
+    )
 
     if (Response.status == 200) {
       return Response.data
