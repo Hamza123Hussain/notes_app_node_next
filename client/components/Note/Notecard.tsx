560288342ae5f0dbd3b342eefad546625acb7c1a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 15:23:46 +0500

    Added Dialog inside the notecard

--- client/components/Note/Notecard.tsx
+++ client/components/Note/Notecard.tsx
@@ -1,41 +1,33 @@
 'use client'
 import React from 'react'
-import { AiOutlineDelete } from 'react-icons/ai'
-import UpdateBtn from './UpdateBtn'
 import { Note } from '@/utils/NoteInterface'
-import { DeleteNote } from '@/functions/DeleteNote'
-import toast from 'react-hot-toast'
-import { useDispatch } from 'react-redux'
-import { RemoveNote } from '@/Redux/NoteSlice'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import NoteTrigger from './NoteTrigger'
+import NoteBody from './NoteBody'
 
 const Notecard = ({ Note }: { Note: Note }) => {
-  const Dispatch = useDispatch()
-  const DeleteMe = async () => {
-    const Removed = await DeleteNote(Note._id)
-
-    if (Removed) {
-      Dispatch(RemoveNote(Note._id))
-      toast.success('NOTE HAS BEEN DELETED')
-    }
-  }
   return (
-    <div
-      style={{ backgroundColor: `${Note.BG}` }}
-      className=" flex flex-col hover:shadow-sm hover:shadow-amber-900  justify-between  h-[30vh] bg-white border-2 border-slate-400  rounded-2xl "
-    >
-      <div>
-        <h1 className=" px-3 py-1 text-xl text-slate-400">{Note.title}</h1>
-        <h3 className=" px-4 text-slate-800">{Note.description}</h3>
-      </div>
-      <div className=" flex justify-end p-2 gap-3 items-center">
-        <UpdateBtn Note={Note} />
-        <AiOutlineDelete
-          onClick={DeleteMe}
-          className=" text-red-500 cursor-pointer"
-          size={20}
-        />{' '}
-      </div>
-    </div>
+    <Dialog>
+      <DialogTrigger>
+        <NoteTrigger Note={Note} />
+      </DialogTrigger>
+      <DialogContent className=" bg-white">
+        <DialogHeader>
+          <DialogTitle>Update Note</DialogTitle>
+          <DialogDescription>
+            <NoteBody Note={Note} />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
 
