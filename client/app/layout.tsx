25341c104750100e4153d799a39b601942c51978
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 15:24:48 +0500

    meta data added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,10 +1,9 @@
-import Footer from '@/components/Footer'
 import './globals.css'
-import Navbar from '@/components/Navbar'
 import Providers from '@/Redux/Provider'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast'
 import CondtionalLayout from '@/components/CondtionalLayout'
+import Head from 'next/head'
 
 export default function RootLayout({
   children,
@@ -13,6 +12,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>NoteSecure - Your Personal Notes</title>
+        <meta
+          name="description"
+          content="A simple, intuitive, and beautiful note-taking app."
+        />
+        <meta
+          name="keywords"
+          content="notes, note-taking, productivity, personal notes, digital notes"
+        />
+        <meta name="author" content="NoteSecure Team" />
+        <meta property="og:title" content="NoteSecure - Your Personal Notes" />
+        <meta
+          property="og:description"
+          content="A simple, intuitive, and beautiful note-taking app."
+        />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <ClerkProvider>
           <Providers>
