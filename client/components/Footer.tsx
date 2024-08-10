50f0a310a9063e2ab79c9e4f76da1020386febd2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 15:25:47 +0500

    adjusted padding and color scheme of footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -4,13 +4,9 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex flex-col md:flex-row justify-between bg-slate-100 shadow-md shadow-indigo-100 p-2">
-      <div className="container mx-auto text-center md:text-left">
-        <div className="flex flex-col md:flex-row justify-center items-center">
-          <p className="text-gray-900">
-            © 2024 NoteSecure. All rights reserved.
-          </p>
-        </div>
+    <footer className="bg-white shadow-md shadow-blue-200 py-1">
+      <div className="container mx-auto text-center">
+        <p className="text-blue-500">© 2024 NoteSecure. All rights reserved.</p>
       </div>
     </footer>
   )
