c7eaab10c5fe39bee1f6f5fdc3302f63e5d6f0af
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 15:25:29 +0500

    added details for no notes present

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,7 +7,7 @@ import { GetNotes } from '@/functions/GetNotes'
 import { SetNote } from '@/Redux/NoteSlice'
 import { RootState } from '@/Redux/Store'
 import { Note } from '@/utils/NoteInterface'
-
+import { FaStickyNote } from 'react-icons/fa' // Import an icon from react-icons
 import { useUser } from '@clerk/nextjs'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -42,11 +42,23 @@ export default function Home() {
       <div className="mt-5 px-4 cursor-pointer">
         <CreateBTN />
       </div>
-      <div className="grid xs:grid-cols-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-4 py-10 gap-6">
-        {Notes.map((note: Note) => (
-          <Notecard Note={note} key={note._id} />
-        ))}
-      </div>
+      {Notes.length > 0 ? (
+        <div className="grid xs:grid-cols-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-4 py-10 gap-6">
+          {Notes.map((note: Note) => (
+            <Notecard Note={note} key={note._id} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center py-20">
+          <FaStickyNote className="text-blue-500 text-6xl mb-4" />
+          <h2 className="text-gray-700 text-xl font-semibold">
+            No notes available
+          </h2>
+          <p className="text-gray-500 mt-2  text-[12px] sm:text-base">
+            Start by creating a new note to keep track of your ideas.
+          </p>
+        </div>
+      )}
     </div>
   )
 }
