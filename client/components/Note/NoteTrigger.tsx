f4bf88d529a024dca7e55cd0d36519eb3e228003
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 16:23:52 +0500

    fixed the card height issue

--- client/components/Note/NoteTrigger.tsx
+++ client/components/Note/NoteTrigger.tsx
@@ -5,7 +5,7 @@ const NoteTrigger = ({ Note }: { Note: Note }) => {
   return (
     <div
       style={{ backgroundColor: `${Note.BG}` }}
-      className=" flex flex-col hover:shadow-sm hover:shadow-amber-900 justify-start h-[30vh] bg-white border-2 border-slate-400  rounded-2xl items-start "
+      className=" flex flex-col hover:shadow-sm hover:shadow-amber-900 justify-start h-[25vh] overflow-hidden   bg-white border-2 border-slate-400 hover:overflow-scroll rounded-2xl items-start "
     >
       <h1 className=" py-1 px-2 text-xl text-slate-400">{Note.title}</h1>
       <h3 className=" px-5 text-slate-800 text-justify ">{Note.description}</h3>
