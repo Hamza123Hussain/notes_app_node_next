071b01528c53c16bb347459e700adb5dee934b18
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 05:03:12 +0500

    justified content and removed unknown files

--- NodeBackend/Config.js
+++
@@ -1,3 +0,0 @@
-import 'dotenv/config'
-export const Port = process.env.Port || 8000
-export const DB = process.env.DB_URL
--- client/components/Note/NoteTrigger.tsx
+++ client/components/Note/NoteTrigger.tsx
@@ -8,7 +8,7 @@ const NoteTrigger = ({ Note }: { Note: Note }) => {
       className=" flex flex-col hover:shadow-sm hover:shadow-amber-900 justify-start h-[30vh] bg-white border-2 border-slate-400  rounded-2xl items-start "
     >
       <h1 className=" py-1 px-2 text-xl text-slate-400">{Note.title}</h1>
-      <h3 className=" px-5 text-slate-800">{Note.description}</h3>
+      <h3 className=" px-5 text-slate-800 text-justify ">{Note.description}</h3>
     </div>
   )
 }
